ab8cec5ebcf2eb0952d56d765b3e26c7630afc34
rbnetomottu <roberto.neto@mottu.com.br>
2025-12-03 14:59:36 -0300

    refactoring PerfomanceCalculator class.

--- createStatementData.js
+++ createStatementData.js
@@ -1,3 +1,56 @@
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount() {
+        let result = 0;
+        switch (this.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${this.play.type}`);
+        }
+        return result;
+    }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.play.type)
+            result += Math.floor(this.performance.audience / 5);
+        return result;
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`unknown type: ${aPlay.type}`);
+    }
+}
+
+
 function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
@@ -7,10 +60,11 @@ function createStatementData(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
@@ -30,32 +84,8 @@ function createStatementData(invoice, plays) {
         return result;
     }
 
-
-
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result;
     }
 }
 
--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 // Importação dos módulos necessários
 const fs = require('fs');
 const path = require('path');
-const { statement } = require('./statement.js');
+const { statement, htmlStatement } = require('./statement.js');
 
 // Lendo os arquivos JSON
 const invoices = JSON.parse(fs.readFileSync(path.join(__dirname, 'invoices.json'), 'utf8'));
@@ -10,6 +10,12 @@ const plays = JSON.parse(fs.readFileSync(path.join(__dirname, 'plays.json'), 'ut
 // Executando a função para cada fatura
 for (const invoice of invoices) {
   const result = statement(invoice, plays);
+  console.log(result);
+  console.log('-----------------------------------');
+}
+// Executando a função para cada fatura no formato HTML
+for (const invoice of invoices) {
+  const result = htmlStatement(invoice, plays);
   console.log(result);
   console.log('-----------------------------------');
 }
